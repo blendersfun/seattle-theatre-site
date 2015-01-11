47dcc8bc7da3cdf779f39275e16c6eee94db243b
Aaron Moore <two.a.ron@gmail.com>
2015-01-11 14:29:45 -0800

    Ripping out everything that will be replaced when integrating fluxible-app and bringing the repo "up to code". :P

--- client/navigation/PageRegistry.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	pages: {},
-	registerPage: function (name, pageComponentFactory) {
-		this.pages[name] = pageComponentFactory;
-	},
-	getPage: function (name) {
-		return this.pages[name] || null;
-	}
-};
